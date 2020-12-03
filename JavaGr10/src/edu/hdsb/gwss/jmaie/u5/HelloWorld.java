25a2fc3307606d471d6d4f28a9b2ca645b188cb7
penguincookies <jamiesumin@gmail.com>
2020-12-02 20:14:39 -0500

    Java

---
+++ JavaGr10/src/edu/hdsb/gwss/jmaie/u5/HelloWorld.java
@@ -0,0 +1,27 @@
+/*
+ * Name: Jamie
+ * Date: 12/10/18
+ * Version: v0.1
+ * Say hello to the world!
+ */
+package edu.hdsb.gwss.jmaie.u5;
+
+/**
+ *
+ * @author 1yijam
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World!");
+        
+        
+        
+    }
+    
+}
